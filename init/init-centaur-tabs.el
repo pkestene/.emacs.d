7b7f693dc7821631b837980406adf4176a5a3de3
Pierre Kestener <pkestene@cea.fr>
2020-08-24 09:57:59 +0200

    minor changes in centaur-tabs config

--- init/init-centaur-tabs.el
+++ init/init-centaur-tabs.el
@@ -6,18 +6,22 @@
   :ensure t
   :config
   (setq centaur-tabs-style "bar"
-        centaur-tabs-set-bar 'over
+        centaur-tabs-set-bar 'under
         centaur-tabs-set-icons t
         centaur-tabs-gray-out-icons 'buffer
         centaur-tabs-height 24
         ;;centaur-tabs-show-navigation-buttons t
         centaur-tabs-set-modified-marker t
         centaur-tabs-modifier-marker "*")
-  (centaur-tabs-enable-buffer-reordering)
+        )
+  ;;(centaur-tabs-enable-buffer-reordering)
   ;; Move the currently selected tab to the left of the the last visited tab.
-  (setq centaur-tabs-adjust-buffer-order 'left)
-  ;;(setq centaur-tabs-adjust-buffer-order t)
+  ;;(setq centaur-tabs-adjust-buffer-order 'left)
+  (setq centaur-tabs-adjust-buffer-order t)
   (centaur-tabs-mode t)
+  :bind
+  ("C-<prior>" . centaur-tabs-backward)
+  ("C-<next>" . centaur-tabs-forward)
   )
 
 (provide 'init-centaur-tabs)
